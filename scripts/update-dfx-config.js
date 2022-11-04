8a8d5d59bffcc0b11969453a1a0730a0af950472
Henry Chan <henry@kinwo.net>
2022-11-04 15:13:45 +1000

    [Ledger] Add update dfx cfg for test

---
+++ scripts/update-dfx-config.js
@@ -0,0 +1,21 @@
+const DFXFile = "../dfx.json"
+const dfxJSON = require(DFXFile)
+const fs = require("fs").promises
+
+const OutputDFXFile = DFXFile
+
+async function main() {
+  dfxJSON["canisters"]["ledger"]["candid"] =
+    "backend/remote/ledger/ledger.private.did"
+
+  try {
+    const content = JSON.stringify(dfxJSON)
+    await fs.writeFile(OutputDFXFile, content)
+    console.log("Test canisters are added successfully.")
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+main()
